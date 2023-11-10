198b02a6f19fdab565116fa3125d34ab0c624cb7
YashBharambay <ybharambay@gmail.com>
2023-11-10 18:12:58 -0500

    test --coverage

--- tests/patientDetailGetRoute.test.js
+++ tests/patientDetailGetRoute.test.js
@@ -6,20 +6,20 @@ describe('GET route tests', () => {
 		const response = await request(app).get('/patients');
 		console.log(response.status);
 		expect(response.statusCode).toBe(200);
-	},30000);
+	},50000);
 
 	it('should handle errors and return a 404 status code for an invalid route', async () => {
 		const response = await request(app).get('/doctors');
 		expect(response.status).toBe(404);
-	},30000);
+	},50000);
 
 	it('should return a 200 status code and valid response for GET route', async () => {
 		const response = await request(app).get('/patients/rydhamik@gmail.com');
 		expect(response.status).toBe(200);
-	},30000);
+	},50000);
 
 	it('should handle errors and return a 404 status code for an invalid route', async () => {
 		const response = await request(app).get('/doctors/rydhamik@gmail.com');
 		expect(response.status).toBe(404);
-	},30000);
+	},50000);
 });
