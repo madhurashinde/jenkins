4d512cfb54f73592ee3bc8a1f4f8fb67d98114e7
YashBharambay <ybharambay@gmail.com>
2023-11-10 20:19:10 -0500

    Get routes

--- tests/patientDetailGetRoute.test.js
+++ tests/patientDetailGetRoute.test.js
@@ -5,20 +5,20 @@ describe('GET route tests', () => {
 	it('should return a 200 status code and valid response for GET route', async () => {
 		const response = await request(app).get('/patients');
 		expect(response.statusCode).toBe(200);
-	},50000);
+	});
 
 	it('should handle errors and return a 404 status code for an invalid route', async () => {
 		const response = await request(app).get('/doctors');
 		expect(response.status).toBe(404);
-	},50000);
+	});
 
-	it('should return a 200 status code and valid response for GET route', async () => {
-		const response = await request(app).get('/patients/rydhamik@gmail.com');
-		expect(response.status).toBe(200);
-	},50000);
+	// it('should return a 200 status code and valid response for GET route', async () => {
+	// 	const response = await request(app).get('/patients/rydhamik@gmail.com');
+	// 	expect(response.status).toBe(200);
+	// },50000);
 
-	it('should handle errors and return a 404 status code for an invalid route', async () => {
-		const response = await request(app).get('/doctors/rydhamik@gmail.com');
-		expect(response.status).toBe(404);
-	},50000);
+	// it('should handle errors and return a 404 status code for an invalid route', async () => {
+	// 	const response = await request(app).get('/doctors/rydhamik@gmail.com');
+	// 	expect(response.status).toBe(404);
+	// },50000);
 });
