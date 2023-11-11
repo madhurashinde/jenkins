814ec8a50d0f36fbe3c8efaceb4f83d0aaeae2ff
YashBharambay <ybharambay@gmail.com>
2023-11-11 13:48:32 -0500

    Added 50000 to get route

--- tests/patientDetailGetRoute.test.js
+++ tests/patientDetailGetRoute.test.js
@@ -5,7 +5,7 @@ describe('GET route tests', () => {
 	it('should return a 200 status code and valid response for GET route', async () => {
 		const response = await request(app).get('/patients');
 		expect(response.statusCode).toBe(200);
-	});
+	},50000);
 
 	it('should handle errors and return a 404 status code for an invalid route', async () => {
 		const response = await request(app).get('/doctors');
