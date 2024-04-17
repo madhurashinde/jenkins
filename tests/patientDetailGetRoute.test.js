bc0f63e6ae4256573d372d001a4c98e984a77803
Madhura Shinde <smadhura23@gmail.com>
2024-04-16 21:28:10 -0400

    Update patientDetailGetRoute.test.js

--- tests/patientDetailGetRoute.test.js
+++ tests/patientDetailGetRoute.test.js
@@ -3,10 +3,10 @@ const app = require('../app');
 
 // Testnig workflow
 describe('GET route tests', () => {
-	// it('should return a 200 status code and valid response for GET route', async () => {
-	// 	const response = await request(app).get('/patients');
-	// 	expect(response.statusCode).toBe(200);
-	// },50000);
+	it('should return a 200 status code and valid response for GET route', async () => {
+		const response = await request(app).get('/patients');
+		expect(response.statusCode).toBe(200);
+	},50000);
 
 	it('should handle errors and return a 404 status code for an invalid route', async () => {
 		const response = await request(app).get('/doctors');
